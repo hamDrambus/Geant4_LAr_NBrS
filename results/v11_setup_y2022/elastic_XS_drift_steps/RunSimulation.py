50bad6ec946d33295d8772ebef61a91ef53030c7
geffdroid <geffdroid@gmail.com>
2022-11-02 15:54:06 +0700

    Added simulation with different drift steps.

---
+++ results/v11_setup_y2022/elastic_XS_drift_steps/RunSimulation.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+import sys
+import os
+import subprocess
+from subprocess import PIPE
+import shutil
+import fileinput
+
+current_path = os.path.dirname(os.path.realpath(__file__))
+parent_path = os.path.dirname(current_path)
+sys.path.append(parent_path)
+
+import SolveFields
+
+# These folders are relative to this script
+ResultsFolder = "./"
+SettingsTemplate = "./settings_template.xml"
+Binary = "../../../../NBrS_THGEM_LAr_v0-build/RelWithDebInfo/Geant_simulation"
+DataPath = "../../../data"
+RecalculateField = False
+RecalculateMesh = False
+Vt1s = [2250, 2250, 2250, 2250, 2250, 2250, 2250, 2250, 2250]
+V0s = [13.0, 13.0, 13.0, 13.0, 13.0, 13.0, 13.0, 13.0, 13.0]
+DSs = [0.1, 0.5, 1.0, 2.0, 4.0, 7.0, 10.0, 15.0, 20.0]
+
+# returns binary absolute path, settings absolute path and log file absolute path (str1, str2, str3)
+def prepare_settings(V0, Vth1, DrSt):
+    path = os.path.abspath(__file__)
+    dir_path = os.path.dirname(path)
+    abs_binary = os.path.normpath(os.path.join(dir_path, Binary))
+    if not os.path.isfile(abs_binary):
+        print ("ERROR: \""+abs_binary+"\" binary does not exist. Cannot run simulation, aborting.")
+        return None
+    abs_binary_path = os.path.dirname(abs_binary)
+    abs_set_templ = os.path.normpath(os.path.join(dir_path, SettingsTemplate))
+    if not os.path.isfile(abs_set_templ):
+        print ("ERROR: \""+abs_set_templ+"\" settings template does not exist. Cannot set up simulation, aborting.")
+        return None
+
+    global RecalculateMesh
+    field_map_files = SolveFields.solve_fields(V0, Vth1, recalculateField=RecalculateField, recalculateMesh=RecalculateMesh)
+    if field_map_files is None:
+        return None
+    RecalculateMesh = False # Mesh needs to be recalulated only once
+
+    suffix = str(round(DrSt, 1)) + "um"
+    abs_output_path = os.path.normpath(os.path.join(os.path.join(dir_path, ResultsFolder), suffix))
+    os.makedirs(abs_output_path, exist_ok=True)
+    abs_settings = os.path.normpath(os.path.join(os.path.join(dir_path, ResultsFolder), "settings_" + suffix + ".xml"))
+    shutil.copy2(abs_set_templ, abs_settings)
+    abs_data_path = os.path.normpath(os.path.join(dir_path, DataPath))
+    rel_data_path = os.path.relpath(abs_data_path, abs_binary_path) # data path/folder is relative to folder from with binary is to be executed!
+    rel_data_path = rel_data_path + "/" # Folders are stored with '/' in my c++ program
+    rel_output_path = os.path.relpath(abs_output_path, abs_binary_path) # output path/folder is relative to folder from which binary is to be executed!
+    rel_output_path = rel_output_path + "/"
+
+    rel_mesh_path = os.path.relpath(field_map_files[0], abs_data_path) # The rest of folders in settings are relative to data folder
+    rel_mesh_path = rel_mesh_path + "/"
+    rel_field_map_file = os.path.relpath(field_map_files[1], abs_data_path)
+
+    for line in fileinput.input([abs_settings], inplace=True):
+        l = line.replace('DATA_PATH', rel_data_path) # binary is executed from its folder
+        l = l.replace('OUTPUT_FOLDER', rel_output_path)
+        l = l.replace('MESH_FOLDER', rel_mesh_path)
+        l = l.replace('DRIFT_STEP', str(round(DrSt, 1)))
+        print(l.replace('FIELD_MAP_FILE', rel_field_map_file), end='')
+    abs_logfile = os.path.join(abs_output_path, "Log.txt")
+    return (abs_binary, abs_settings, abs_logfile)
+
+
+if __name__ == "__main__":
+    if len(V0s) != len(Vt1s) or len(V0s) != len(DSs) :
+        print("ERROR: V0s, Vt1s and DSs have diffrent lengths (" + len(V0s) + ", " + len(Vt1s) + ", " + len(DSs) + "). Aborting RunSimulation.py.")
+        sys.exit(1)
+    for i in range(min(len(V0s), len(Vt1s))):
+        print ("****************************************************************")
+        print ("****************************************************************")
+        print ("Starting simulating V0 = " + str(round(V0s[i], 1)) + ", Vthgem = " + str(round(Vt1s[i])) + ", Drift_step = " + str(round(DSs[i], 1)) )
+        print ("****************************************************************")
+        print ("****************************************************************")
+        files = prepare_settings(V0s[i], Vt1s[i], DSs[i])
+        if files is None:
+            continue
+        abs_binary_path = os.path.dirname(files[0])
+        binary = os.path.basename(files[0])
+        binary = os.path.join("./", binary)
+
+        p1 = subprocess.Popen([binary, files[1]], stdout=PIPE, stderr=subprocess.STDOUT, cwd=abs_binary_path)
+        p2 = subprocess.Popen(["tee", files[2]], stdin=p1.stdout)
+        p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
+        p2.communicate()
+        if not p1.returncode is None:
+            print ("****************************************************************")
+            print ("****************************************************************")
+            print("ERROR while running " + binary + ". Skipping V0= " + str(round(V0s[i], 1)) + ", Vthgem= " + str(round(Vt1s[i])) + ", Drift_step = " + str(round(DSs[i], 1)) )
+            print ("****************************************************************")
+            print ("****************************************************************")
+            continue
+        print ("****************************************************************")
+        print ("****************************************************************")
+        print ("Ended simulating V0 = " + str(round(V0s[i], 1)) + ", Vthgem = " + str(round(Vt1s[i])) + ", Drift_step = " + str(round(DSs[i], 1)) )
+        print ("****************************************************************")
+        print ("****************************************************************")
+    print("End of RunSimulation.py")
